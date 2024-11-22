19845449f1fa7a84b5e8fa7c4fd24ac2a4d7215f
Dan Patrascu-Baba <dan@danpatrascu.com>
2024-11-22 20:58:45 +0200

    Initial commit

---
+++ Codewrinkles.Evolve/Evolve.Api/Auth/AccountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Evolve.Api.Auth;
+
+[ApiController]
+public class AccountsController : ControllerBase
+{
+
+    [HttpPost]
+    [Route("api/signup")]
+    public async Task<IActionResult> Register()
+    {
+
+        return Ok();
+    }
+}
